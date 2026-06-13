a9fad95e5a830947003bd5be016916355e846892
ghouul420 <levipaulmueller@outlook.de>
2026-06-14 00:59:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 const { Player } = require('discord-player');
 const { DefaultExtractors } = require('@discord-player/extractor');
 const { VoiceConnectionStatus } = require('@discordjs/voice');
+const ffmpeg = require('ffmpeg-static');
 
 //uptime
 
@@ -54,6 +55,8 @@ const player = new Player(client, {
         defaultBridge: "soundcloud",
     }        
 });
+
+process.env.DP_FORCE_FFMPEG = ffmpeg;
 
 player.extractors.loadMulti(DefaultExtractors);
  
