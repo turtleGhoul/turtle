3072452cdd9a7260721eb029cd5d179c1b1ca67b
ghouul420 <levipaulmueller@outlook.de>
2026-06-14 13:56:34 +0200

    Update speak.js

--- commands/slash/speak.js
+++ commands/slash/speak.js
@@ -1,6 +1,6 @@
-const { SlashCommandBuilder } = require('discord.js');
-const googleTTS  = require('google-tts-api');
-const { createAudioPlayer, createAudioResource, joinVoiceChannel, AudioPlayerStatus, entersState, VoiceConnectionStatus } = require('@discordjs/voice');
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
+const googleTTS = require('google-tts-api');
+const { createAudioPlayer, createAudioResource, joinVoiceChannel, AudioPlayerStatus, entersState, VoiceConnectionStatus, StreamType } = require('@discordjs/voice');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -13,26 +13,23 @@ module.exports = {
         ),
 
     async execute(interaction) {
-
-        await interaction.deferReply({ ephemeral: true });
+        await interaction.deferReply({ flags: [MessageFlags.Ephemeral] });
 
         const voiceChannel = interaction.member.voice.channel;
         const text = interaction.options.getString('text');
 
         if (!voiceChannel) {
-
             return interaction.editReply({ content: 'musst in nem voice sein' });
         }
 
         if (text.length > 200) {
-
             return interaction.editReply({ content: 'der text ist zu lang, bitte kürze ihn auf 200 zeichen' });
         }
 
         const url = googleTTS.getAudioUrl(text, {
             lang: 'de',
             slow: false,
-            host: 'https://translate.google.com',
+            host: 'https://google.com',
         }); 
 
         const connection = joinVoiceChannel({
@@ -42,31 +39,31 @@ module.exports = {
         });
 
         try {
-           
             await entersState(connection, VoiceConnectionStatus.Ready, 5000);
 
             const player = createAudioPlayer();
-            const resource = createAudioResource(url);
+            const resource = createAudioResource(url, {
+                inputType: StreamType.Arbitrary
+            });
 
             connection.subscribe(player);
             player.play(resource);
 
-            await interaction.followUp({ content: 'yap yap', ephemeral: true });
+            await interaction.followUp({ content: 'yap yap', flags: [MessageFlags.Ephemeral] });
 
             player.on(AudioPlayerStatus.Idle, () => {
                 connection.destroy();
             });
 
             player.on('error', error => {
-                console.error('Fehler beim AudioPlayer:', error);
+                console.error(error);
                 connection.destroy();
             });
 
         } catch (error) {
-            console.error("Fehler im Try-Block:", error);
+            console.error(error);
             connection.destroy();
-            await interaction.followUp({ content: 'whoopsie.. etwas ist schiefgelaufen ', ephemeral: true });
+            await interaction.followUp({ content: 'whoopsie.. etwas ist schiefgelaufen', flags: [MessageFlags.Ephemeral] });
         }
-        
     },
 };
